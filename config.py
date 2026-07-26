08b363619cd1f1b339c77a7aa22972fcb146de43
Stevesuk <stevesukawa@outlook.com>
2026-07-27 01:47:31 +0900

    update config

---
+++ config.py
@@ -0,0 +1,25 @@
+# API server endpoint URL. Require an OpenAI-compatible endpoint:
+base_url = ''
+
+# API authentication key. Used to verify your identity when calling the API.
+api_key = ''
+
+# Model displaying name. Specifies which AI model in system prompt and UI.
+model = ''
+
+# Model identifier id. Specifies which AI model in requests.
+model_id = ''
+
+
+# ==========================
+# UI Configuration
+# ==========================
+
+# Whether to enable the alternative theme.
+#
+# False:
+#   Use the default color theme: Magentic
+#
+# True:
+#   Switch to the alternate theme: Ocean
+use_alt_theme = False
